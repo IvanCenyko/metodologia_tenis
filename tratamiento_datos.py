05701b8fb1a356d39b8b7e7b67cd78ccdfec07f4
IvanCenyko <ivancenyko@gmail.com>
2024-10-23 12:51:10 -0300

    comments, clean main

--- tratamiento_datos.py
+++ tratamiento_datos.py
@@ -17,39 +17,40 @@ for archivo in os.listdir(carpeta_csv):
         dataframes_archivos[archivo.replace(".csv", '')] = df
 
 
-
+# tomo columnas de algun dataframe (todos son iguales en formato)
 columnas = list(dataframes_archivos["atp_matches_2004"])
 
-
+# dataframe vacio con partidos nadal
 partidos_nadal = pd.DataFrame(columns=columnas)
+# dataframe vacio con partidos djokovic
 partidos_djokovic = pd.DataFrame(columns=columnas)
 
 
-
-
-
+# rango de agnos a revisar
 years = range(2004, 2023)
 
+# datos de Djokovic
 
-
+# para cada agno
 for year in years:
 
+    # tomo el dataframe de ese agno
     frame = dataframes_archivos[f"atp_matches_{str(year)}"]
+    # tomo partidos que gano
     winner = frame[(frame['winner_name'].str.contains("Novak Djokovic"))]
+    # tomo partidos que perdio
     loser = frame[(frame['loser_name'].str.contains("Novak Djokovic"))]
 
-
+    # concateno ambos
     total = pd.concat([winner, loser], ignore_index=True, sort=False)
 
+    # contateno en dataframe de todos los partidos, los que lei de este agno
     partidos_djokovic = pd.concat([total, partidos_djokovic], ignore_index=True, sort=False)
 
 
-
-
-
+# idem para Nadal
 
 years = range(2004, 2023)
-
 
 
 for year in years:
@@ -63,8 +64,6 @@ for year in years:
 
 
     partidos_nadal = pd.concat([total, partidos_nadal], ignore_index=True, sort=False)
-
-
 
 
 
